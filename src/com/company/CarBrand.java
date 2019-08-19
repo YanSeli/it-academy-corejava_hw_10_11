6e4c021d44f05e4e5cffe54232ca57d348c862e1
Ян Селиванов <Y.Selivanau@NovaCom.by>
2019-08-19 16:18:29 +0300

    Initial commit

---
+++ src/com/company/CarBrand.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public enum CarBrand {
+        AUDI,
+        BMW,
+        MERCEDES
+    }
+    enum СarModelAudi{
+        A1, A2, A3, A4, A5, A6, A7, A8, Q3, Q5, Q7, Q8
+    }
+    enum CarModelBmw{
+        SERIES_1, SERIES_2, SERIES_3, SERIES_4, SERIES_5, SERIES_6, SERIES_7,
+        X_1, X_2, X_3, X_4, X_5, X_6, X_7
+    }
+    enum CarModelMercedes{
+        C_CLASS, E_CLASS, S_CLASS, G_CLASS, ML_CLASS, GL_CLASS
+    }
+}
